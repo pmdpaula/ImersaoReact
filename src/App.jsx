e395d7e892b45ee37067b6312d5b159e9b290b83
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-07-27 22:46:28 -0300

    Aula 01 com desafios

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,29 @@ import dadosIniciais from './data/dados_iniciais.json'
 
 
 function App() {
+
   return (
     <div style={{background: "#141414"}} >
       <Menu />
-      
+
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={`O que é Front-End? ...`}
       />
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+
+      {
+      dadosIniciais.categorias.map((category, index) => {
+        return (
+          <Carousel
+            key={index}
+            // ignoreFirstVideo
+            category={category}
+            // category={dadosIniciais.categorias[0]}
+          />
+        )
+      })
+      }
       <Footer
         category={dadosIniciais.categorias[1]}
       />
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../assets/axetech01-lg.png';
+import Logo from '../../assets/axethings-md.png';
 import './Menu.css';
 import ButtonLink from './components/ButtonLink/ButtonLink';
 import Button from '../Button/Button';
@@ -8,10 +8,10 @@ export default () => {
   return (
     <nav className="Menu" >
       <a href="/" >
-        <img className="Logo" src={Logo} alt="AxeTech Logo" />
+        <img className="Logo" src={Logo} alt="AxeThings Logo" />
       </a>
       
-      <Button as="a" href="/" className="ButtonLink" textButton="Novo VÃ­deo" >
+      <Button as="a" href="/" className="ButtonLink" textButton="Novo Ví­deo" >
         Novo Vídeo
       </Button>
     </nav>
