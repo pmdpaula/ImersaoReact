18fff413e7cfe6145c6c0e3963775d745b493782
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-07-30 22:38:09 -0300

    Ajuste o botão do menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,20 +5,18 @@ import './Menu.css';
 import Logo from '../../assets/axethings-02-md.png';
 import ButtonEffect from '../ButtonEffect/ButtonEffect';
 
-export default () => {
-  return (
-    <nav className="Menu" >
-      <Link to="/" >
-        <img className="Logo" src={Logo} alt="AxeThings Logo" />
-      </Link>
-      
-      {/* <Button as={Link} to="/cadastro/video" >
+export default () => (
+  <nav className="Menu">
+    <Link to="/">
+      <img className="Logo" src={Logo} alt="AxeThings Logo" />
+    </Link>
+
+    {/* <Button as={Link} to="/cadastro/video" >
         Novo Vídeo
       </Button> */}
 
-        <ButtonEffect dark color="primary">
-          <span>Novo Vídeo</span>
-        </ButtonEffect>
-    </nav>
-  )
-}
+    <ButtonEffect as={Link} to="/cadastro/video" bgcolor="dark" color="primary">
+      <span>Novo Vídeo</span>
+    </ButtonEffect>
+  </nav>
+);
